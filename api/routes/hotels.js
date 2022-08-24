d2bfb16fe43ddbb346a4bef39434f6a33a4bc53f
jawad <jawad@xeventechnologies.com>
2022-08-25 00:39:01 +0500

    CRUD completed of hotels

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -11,19 +11,37 @@ router.post("/", async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-  res.send("Welcome to hotels");
 });
 
 //UPDATE HOTEL
-router.put("/:id", async (req, res) => {
+router.delete("/:id", async (req, res) => {
   try {
-    const updatedHotel = await Hotel.findByIdAndUpdate(req.params.id, {
-      $set: req.body,
-    });
-    res.status(200).json(updatedHotel);
+    const deletedHotel = await Hotel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Hotel Deleted success fully");
   } catch (err) {
     res.status(500).json(err);
   }
-  res.send("Welcome to hotels");
 });
+//GET BY ID
+
+router.get("/:id", async (req, res) => {
+  try {
+    const findAHotel = await Hotel.findById(req.params.id);
+    res.status(200).json(findAHotel);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// GET ALL HOTELS
+
+router.get("/", async (req, res) => {
+  try {
+    const getAllHotels = await Hotel.find();
+    res.status(200).json(getAllHotels);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 export default router;
