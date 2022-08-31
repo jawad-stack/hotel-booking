a8423269986bb07a2d5c4032026181a051f653d0
jawad <jawad@xeventechnologies.com>
2022-09-01 00:43:18 +0500

    Authentication and user, hotel crud authentication

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -6,22 +6,23 @@ import {
   getAllHotels,
   updateHotel,
 } from "../controllers/hotel.js";
+import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 const router = express.Router();
 
 //ADD NEW HOTEL
-router.post("/", createHotel);
+router.post("/", verifyAdmin, createHotel);
 
 //DELETE HOTEL
-router.delete("/:id", deleteHotel);
+router.delete("/:id", verifyAdmin, deleteHotel);
 //GET BY ID AND UPDATE
 
-router.get("/:id", updateHotel);
+router.put("/:id", verifyAdmin, updateHotel);
 //GET BY ID
 
-router.get("/:id", getHotel);
+router.get("/:id", verifyUser, getHotel);
 
 // GET ALL HOTELS
 
-router.get("/", getAllHotels);
+router.get("/", verifyUser, getAllHotels);
 
 export default router;
